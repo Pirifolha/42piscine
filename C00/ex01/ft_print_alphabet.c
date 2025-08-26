cfe5e8e14c6f46b189357c6751ec8c0520f51a90
Miguel Sousa <miguelsousa218@gmail.com>
2025-08-26 19:49:30 +0100

    added C00

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misousa <misousa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 19:43:42 by misousa           #+#    #+#             */
+/*   Updated: 2025/08/02 14:56:38 by misousa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misousa <misousa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 19:54:12 by misousa           #+#    #+#             */
+/*   Updated: 2025/07/31 19:59:43 by misousa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	write (1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
